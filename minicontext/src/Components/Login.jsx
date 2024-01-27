4f2291e31489c2ec9df0398c615573c307810993
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2024-01-27 10:33:12 +0530

    Add files via upload

---
+++ minicontext/src/Components/Login.jsx
@@ -0,0 +1,22 @@
+import React,{useCallback,useContext,useState} from 'react'
+import UserContext from '../Context/UserContext'
+
+function Login() {
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+    
+    const {setUser}=useContext(UserContext)
+    const handleSubmit=()=>{
+        setUser({username,password})
+    }
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" placeholder='username' value={username} onChange={(e)=>setUsername(e.target.value)} />
+      <input type="text" placeholder='password'  onClick={(e)=>setPassword(e.target.value)} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
